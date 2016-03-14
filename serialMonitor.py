ddae829a0109a5ee5f1c89c9d76b7164401e70d5
Micheál <hello@micheallooney.com>
2016-03-14 08:26:55 -0400

    Remove duplicate files

--- serialMonitor.py
+++
@@ -1,7 +0,0 @@
-import serial
-ser = serial.Serial('/dev/tty.usbmodem1411', 9600) # Establish the connection on a specific port at 9600 baud
-
-
-while True: # run forever
-     print ser.readline() # Read the output from the arduino and print it to the console
-     
